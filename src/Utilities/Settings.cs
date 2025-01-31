23b997fd10f17d5841777c436ebd6c1f5fa27302
HAHOOS <hahoos@hahoos.pl>
2025-01-31 16:51:20 +0100

    Add Text Color setting

--- src/Utilities/Settings.cs
+++ src/Utilities/Settings.cs
@@ -3,6 +3,7 @@ using NEP.MagPerception.UI;
 using UnityEngine;
 
 using Tomlet.Attributes;
+using System.Drawing;
 
 namespace NEP.MagPerception
 {
@@ -113,6 +114,51 @@ namespace NEP.MagPerception
             }
         }
 
+        [TomlNonSerialized]
+        public Color32 TextColor
+        {
+            get
+            {
+                var drawingColor = ColorTranslator.FromHtml(TextColorHEX);
+                return new Color32(drawingColor.R, drawingColor.G, drawingColor.B, drawingColor.A);
+            }
+            set
+            {
+                TextColorHEX = $"#{value.r:X2}{value.g:X2}{value.b:X2}";
+            }
+        }
+
+        [TomlNonSerialized]
+        private string _textColorHEX = "#FFFFFF";
+
+        [TomlProperty("TextColor")]
+        public string TextColorHEX
+        {
+            get => _textColorHEX;
+            set
+            {
+                _textColorHEX = value;
+                Main.PrefsCategory.SaveToFile(false);
+            }
+        }
+
+        public void ChangeHSV(HSVValue hsvValue, float value)
+        {
+            UnityEngine.Color.RGBToHSV(TextColor, out float H, out float S, out float V);
+            switch (hsvValue)
+            {
+                case HSVValue.H:
+                    H = value; break;
+
+                case HSVValue.V:
+                    V = value; break;
+
+                case HSVValue.S:
+                    S = value; break;
+            }
+            TextColor = UnityEngine.Color.HSVToRGB(H, S, V);
+        }
+
         public void ChangeXYZOffset(OffsetValue xyz, float value)
         {
             var offset = Offset;
@@ -133,6 +179,13 @@ namespace NEP.MagPerception
             X,
             Y,
             Z,
+        }
+
+        public enum HSVValue
+        {
+            H,
+            S,
+            V
         }
     }
 }
